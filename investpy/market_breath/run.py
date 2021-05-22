625592982f9cbbcffd34ef86679529439659352c
Fan Zhang <4636221+fzhang00@users.noreply.github.com>
2021-05-22 19:59:36 -0400

    add sector ma cal in run.py

--- investpy/market_breath/run.py
+++ investpy/market_breath/run.py
@@ -5,7 +5,7 @@ To add more tasks to the top runner, import the function and add it to main()
 """
 
 from investpy.market_breath.get_sp500_data import download_Update_SaveData_History_all500
-from investpy.market_breath.sp500MarketBread import updateMarketBreadth_SP500Sector
+from investpy.market_breath.sp500MarketBread import updateMarketBreadth_SP500Sector, calMA_SP500Sector
 from investpy.market_breath.movingAvgCal import overwrite_sp500_prepDirFiles
 import os
 import investpy.market_breath.sp500Const as const
@@ -15,6 +15,7 @@ def main():
         os.mkdir(const.sp500dataWorkingDir_const)
     print("call download_Update_SaveData_History_all500")
     download_Update_SaveData_History_all500 (const.sp500Info_file_const)
+    calMA_SP500Sector(maDay_int = 20, logOn = False)
     overwrite_sp500_prepDirFiles()
     updateMarketBreadth_SP500Sector(20)
 

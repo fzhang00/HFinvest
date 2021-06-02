b702b01a57caaabfa397acf867423b8e0bfb83f3
Fan Zhang <4636221+fzhang00@users.noreply.github.com>
2021-06-01 22:13:49 -0400

    add Fan's config

--- config_start_daily_run.bat
+++ config_start_daily_run.bat
@@ -1,2 +1,7 @@
-@CALL "C:\ProgramData\Anaconda3\Scripts\activate.bat" C:\ProgramData\Anaconda3
-@CALL python G:\Projects\HFinvest\top_daily_run.py
+REM Hao's LAPTOP CONFIG
+REM @CALL "C:\ProgramData\Anaconda3\Scripts\activate.bat" C:\ProgramData\Anaconda3
+REM @CALL python G:\Projects\HFinvest\top_daily_run.py
+
+REM Fan's Laptop config
+@CALL "C:\Users\fzhan\miniconda3\Scripts\activate.bat" invest
+@CALL python C:\Users\fzhan\Projects\MyProjects\Investment\HFinvest\top_daily_run.py

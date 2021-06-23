c66ef809d396a596b2546b79d9a1c724f4d5ac82
Fan Zhang <4636221+fzhang00@users.noreply.github.com>
2021-06-22 23:08:03 -0400

    import from fan_lib, and change column Date to DATE

--- FED/fred2sql.py
+++ FED/fred2sql.py
@@ -1,9 +1,16 @@
+import sys
+sys.path.append("../fan_lib")
 import datetime
 import pandas as pd
 import pandas_datareader as web
 import sqlalchemy
 from sqlalchemy import text, inspect
-from HFinvest import key as pconst
+from invest_db import RYAN_SQL, FAN_SQL, InvestDB
+# from HFinvest import key as pconst
+
+QUANDL_KEY = '_JyFt8HS_T8C4qsXBo68'
+
+BEA_Key = "5FCFE8A8-1714-4DE1-9F49-E8AFA9BEFB93"
 
 _DEFAULT_START_DATE = datetime.datetime(1970,1,1)
 _DEFAULT_END_DATE = datetime.datetime.today()
@@ -33,16 +40,16 @@ def data_to_sql(symbol_list, data_source,
     # Delete the last date record, as it will be re-written. 
     if if_exists == 'append':
         with engine.connect() as conn:
-            query = """SELECT TOP (1) * FROM %s.dbo.%s ORDER BY [Date] DESC;"""%(db,table_name)
-            last_data = pd.read_sql(query, conn, index_col='Date', parse_dates='Date')
-            query = text("DELETE FROM %s.dbo.%s WHERE [Date] = '%s.000'"%(db, table_name, last_data.index[-1]))
+            query = """SELECT TOP (1) * FROM %s.dbo.%s ORDER BY [DATE] DESC;"""%(db,table_name)
+            last_data = pd.read_sql(query, conn, index_col='DATE', parse_dates='DATE')
+            query = text("DELETE FROM %s.dbo.%s WHERE [DATE] = '%s.000'"%(db, table_name, last_data.index[-1]))
             conn.execute(query)
         new_data = web.DataReader(symbol_list, data_source,
                                 last_data.index[-1], end_date, api_key=api_key)
     else: # Not appending, simply do as user asks. 
         new_data = web.DataReader(symbol_list, data_source,
                                 start_date, end_date, api_key=api_key)
-    new_data.to_sql(table_name, engine, if_exists=if_exists, index_label='Date')
+    new_data.to_sql(table_name, engine, if_exists=if_exists, index_label='DATE')
 
 
 
@@ -65,12 +72,12 @@ def csv_to_sql(file_name, engine, table_name, if_exists):
     data.to_sql(table_name, engine, if_exists=if_exists, index=False)
 
 def fred_to_sql(symbol_list, db, table_name, start_date=_DEFAULT_START_DATE,
-                end_date=_DEFAULT_END_DATE):
+                end_date=_DEFAULT_END_DATE, replace=False):
     """Given a list of symbols,  
     Download data using data reader and store them in database. """
     ins = inspect(db.sqlcon)
     db_tables = ins.get_table_names()
-    if table_name in db_tables:
+    if (table_name in db_tables) and (not replace):
         data_to_sql(symbol_list, 'fred', db.sqlcon, db.database,
                     table_name, start_date = _DEFAULT_START_DATE,
                     end_date = _DEFAULT_END_DATE, if_exists='append')
@@ -80,7 +87,7 @@ def fred_to_sql(symbol_list, db, table_name, start_date=_DEFAULT_START_DATE,
                     end_date = _DEFAULT_END_DATE, if_exists='replace')
 
 
-def update_fred_data(db):
+def update_fred_data(db, replace=False):
     """Update FED data using symbol included in FED_US_SYMBOL table
     
     Input:
@@ -93,6 +100,7 @@ def update_fred_data(db):
         # Symbol table not in db table list, add it in.
         # This symbol list is compiled manually from FRED spreadsheet add-in
         csv_to_sql("FRED_symbol.csv", db.sqlcon, _FED_SYMBOL_TABLE, "replace")
+
     # Grab data on Symbol List
     # select distinc category, one category per table
     categories = db.get_distinct_items(_FED_SYMBOL_TABLE, 'Category')
@@ -105,7 +113,7 @@ def update_fred_data(db):
         table_name = _FED_TABLE_PREFIX+'_'.join((c.upper()).split(' '))
         print("Working on table", table_name)
         # Fetch data from FRED and populate sql table. Table will be created if not exists.
-        fred_to_sql(symbol_lst['Symbol'], db, table_name)
+        fred_to_sql(symbol_lst['Symbol'], db, table_name, replace=replace)
     db.close()
 
 def update_fred_symbol_table( db, symbol_file="FRED_symbol.csv"):
@@ -118,11 +126,8 @@ def update_fred_symbol_table( db, symbol_file="FRED_symbol.csv"):
     csv_to_sql("FRED_symbol.csv", db.sqlcon, _FED_SYMBOL_TABLE, "replace")
 
 
-db_info = pconst.RYAN_SQL
-db_info['database'] = "FED"
-db = sqlalchemy.create_engine('mssql+pyodbc://'+db_info['username']+':'+db_info['password']+\
-                            '@' + db_info['server'] + \
-                            '/' + db_info['database'] + \
-                            '?driver=ODBC+Driver+17+for+SQL+Server')
+db_info = FAN_SQL
+# db_info['database'] = "FED"
+db = InvestDB(db_info)
 update_fred_symbol_table(db, "FRED_symbol.csv")
-update_fred_data(db)
+update_fred_data(db, replace=False)

f518a24c6fe47040ceb67a416982426673eb68c7
Fan Zhang <4636221+fzhang00@users.noreply.github.com>
2023-09-23 14:42:12 -0400

    convert string table to number

--- fund_flow/data_writer.py
+++ fund_flow/data_writer.py
@@ -11,8 +11,6 @@ db_info = {'driver': 'ODBC Driver 17 for SQL Server',
             'database':'Ticker' ,
             'username': 'hl',
             'password': '123'} 
-# URL of the webpage with the table
-url = 'https://etfdb.com/etfs/industry/#industry-power-rankings__fund-flow-leaderboard&sort_name=aum_position&sort_order=asc&page=1'
 
 # Establish a connection to the SQL Server database
 conn = sqlalchemy.create_engine('mssql+pyodbc://'+db_info['username']+':'+db_info['password']+\
@@ -28,50 +26,81 @@ def table_exists(conn, table_name):
 
 def get_latest_date(cursor, table_name):
     # Query the latest date for the given symbol
-    query = f"SELECT MAX([Date]) FROM etf_fund_flow"
+    query = f"SELECT MAX([Date]) FROM {table_name}"
     latest_date = cursor.execute(query)
     return latest_date.fetchone()[0]
 
+def download_etfdb_fundflow(url):
+    """Download fund flow data. Raw data are strings. """
+    # Scrape the table from the URL
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table')
 
-# Scrape the table from the URL
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-table = soup.find('table')
-
-# Extract the table headers
-headers = [th.text for th in table.find_all('th')]
-headers = [h.split("\n\n")[1] for h in headers]
-for i in range(len(headers)):
-    if headers[i]=="+/-":
-        headers[i] = " ".join(headers[i-1:i+1])
-
-
-# Extract last updated date
-last_updated=""
-for p in soup.find_all("p"):
-    if "Last updated" in p.text:
-        last_updated = p.text
-publish_date = last_updated.split('Last updated on ')[1].strip("\n")
-publish_date = datetime.strptime(publish_date, '%b %d, %Y').date()
-# Extract the table rows
-rows = []
-for tr in table.find_all('tr'):
-    data = [td.text.strip() for td in tr.find_all('td')]
-    if len(data) == len(headers):
-        rows.append(data)
-df = pd.DataFrame(data=rows, columns=headers)
-df['Date'] = publish_date
-
-# Specify the table name
-table_name = 'etf_fund_flow'
+    # Extract the table headers
+    headers = [th.text for th in table.find_all('th')]
+    headers = [h.split("\n\n")[1] for h in headers]
+    for i in range(len(headers)):
+        if headers[i]=="+/-":
+            headers[i] = " ".join(headers[i-1:i+1])
 
 
-# Check if the table exists in the database
-if not table_exists(conn, table_name):
-    df.to_sql(table_name, conn, if_exists='replace')
-    print("Database doesn't exists. Create and write data")
-elif publish_date > get_latest_date(conn, table_name):
-    df.to_sql(table_name, conn, if_exists='append')
-    print("Append data")
-else:
-    print("Already up to date. ")
+    # Extract last updated date
+    last_updated=""
+    for p in soup.find_all("p"):
+        if "Last updated" in p.text:
+            last_updated = p.text
+    publish_date = last_updated.split('Last updated on ')[1].strip("\n")
+    publish_date = datetime.strptime(publish_date, '%b %d, %Y').date()
+    # Extract the table rows
+    rows = []
+    for tr in table.find_all('tr'):
+        data = [td.text.strip() for td in tr.find_all('td')]
+        if len(data) == len(headers):
+            rows.append(data)
+    df = pd.DataFrame(data=rows, columns=headers)
+    df['Date'] = publish_date
+
+    return df
+
+
+
+def to_sql_table(table_name, df):
+    """Insert df to SQL table. If table exists, append."""
+    # Check if the table exists in the database
+    if not table_exists(conn, table_name):
+        df.to_sql(table_name, conn, if_exists='replace')
+        print("Database doesn't exists. Create and write data")
+    elif publish_date > get_latest_date(conn, table_name):
+        df.to_sql(table_name, conn, if_exists='append')
+        print("Append data")
+    else:
+        print("Already up to date. ")
+
+def convert_to_num(df):
+    """Convert downloaded etf table from string to number.
+    Specifically, convert $xx,xxx.xx to float. xx% to float. 
+    If a cell is not convertable, replace it with NaN. """
+    str_cols = ['Industry', 'Date', 'power_ranking_sort_text']
+    remove_dollar = lambda x: pd.to_numeric(x.replace('$', '').replace(',', ''), errors='ignore')
+    remove_pct = lambda x: pd.to_numeric(x.replace('%',''), errors='ignore')
+    for col in df.columns:
+        if col not in str_cols:
+            
+            if isinstance(df[col][0], str):
+                if '$' in df[col][0]:
+                    df[col] = df[col].apply(remove_dollar)
+                elif '%' in df[col][0]:
+                    df[col] = df[col].apply(remove_pct)
+                else:
+                    df[col] = df[col].apply(pd.to_numeric, errors='coerce')
+    return df
+
+if __name__ == "__main__":
+    # URL of the webpage with the table
+    url = 'https://etfdb.com/etfs/industry/#industry-power-rankings__fund-flow-leaderboard&sort_name=aum_position&sort_order=asc&page=1'
+
+    raw_df = download_etfdb_fundflow(url)
+    to_sql_table('etf_fund_flow', raw_df)
+    df = convert_to_num(raw_df)
+    to_sql_table("etf_fund_flow_num", df)
